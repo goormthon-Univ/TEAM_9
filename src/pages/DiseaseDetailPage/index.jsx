1b753f31fe3e6f4724d9250a8b1fa85b5581809a
lybell <futireflugel@gmail.com>
2023-11-15 00:05:47 +0900

    :sparkles: 질병 상세 페이지에 스켈레톤 UI 추가

--- src/pages/DiseaseDetailPage/index.jsx
+++ src/pages/DiseaseDetailPage/index.jsx
@@ -1,6 +1,7 @@
 import { useMemo, Suspense } from "react";
 import { useParams } from "react-router-dom";
 import DiseaseDetail from "./DiseaseDetail.jsx";
+import DiseaseDetailSkeleton from "./DiseaseDetailSkeleton.jsx";
 import ErrorBoundary from "../../components/ErrorBoundary.jsx";
 import { fetchAxios } from "../../utils/utils.js";
 
@@ -14,7 +15,7 @@ export default function DiseaseDetailPage() {
   return (
     <>
       <ErrorBoundary fallback={<div>페이지를 찾을 수 없음</div>}>
-        <Suspense fallback={<div>로딩중...</div>}>
+        <Suspense fallback={<DiseaseDetailSkeleton />}>
           <DiseaseDetail resource={resource} />
         </Suspense>
       </ErrorBoundary>
