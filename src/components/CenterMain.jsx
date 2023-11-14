a170b66c3e7accb8a367c862b33b4897188c6bf6
lybell <futireflugel@gmail.com>
2023-11-14 23:44:15 +0900

    :lipstick: CenterMain 컴포넌트에 패딩 추가.
    1200px 이하인 페이지에서 더 낫게 바뀌었습니다.

--- src/components/CenterMain.jsx
+++ src/components/CenterMain.jsx
@@ -2,8 +2,10 @@ import styled from "styled-components";
 
 const CenterMain = styled.main`
   width: 100%;
-  max-width: 1180px;
+  max-width: 1200px;
+  box-sizing: border-box;
   margin: 0px auto;
+  padding: 10px;
 `;
 
 export default CenterMain;
