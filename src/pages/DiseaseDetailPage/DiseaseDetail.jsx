4ca29bb17bbd5ed36c627482ed9dcdd9589af1da
lybell <futireflugel@gmail.com>
2023-11-14 23:42:12 +0900

    :lipstick: 메인 폰트를 pretendard로 수정

--- src/pages/DiseaseDetailPage/DiseaseDetail.jsx
+++ src/pages/DiseaseDetailPage/DiseaseDetail.jsx
@@ -27,29 +27,31 @@ const SubTitle = styled.h3`
 export default function DiseaseDetail({ resource }) {
   const data = resource();
 
-  return <CenterMain>
-    <SummeryWrapper>
-      <Pic>(사진, api 미존재)</Pic>
-      <div className="descriptions">
-        <Title>{data.disease_name}</Title>
-        <SubTitle>질병소개</SubTitle>
-        <p>(api 미존재, 백엔드와 상의 예정)</p>
-      </div>
-    </SummeryWrapper>
-    <article>
-      <Title>질병 치료법</Title>
-      <section>
-        <SubTitle>예방법</SubTitle>
-        <p>{data.disease_protect}</p>
-      </section>
-      <section>
-        <SubTitle>치료법</SubTitle>
-        <p>{data.disease_cure}</p>
-      </section>
-      <section>
-        <SubTitle>관련 약품</SubTitle>
-        <p>{data.medicine_name}</p>
-      </section>
-    </article>
-  </CenterMain>;
+  return (
+    <CenterMain>
+      <SummeryWrapper>
+        <Pic>(사진, api 미존재)</Pic>
+        <div className="descriptions">
+          <Title>{data.disease_name}</Title>
+          <SubTitle>질병소개</SubTitle>
+          <p>(api 미존재, 백엔드와 상의 예정)</p>
+        </div>
+      </SummeryWrapper>
+      <article>
+        <Title>질병 치료법</Title>
+        <section>
+          <SubTitle>예방법</SubTitle>
+          <p>{data.disease_protect}</p>
+        </section>
+        <section>
+          <SubTitle>치료법</SubTitle>
+          <p>{data.disease_cure}</p>
+        </section>
+        <section>
+          <SubTitle>관련 약품</SubTitle>
+          <p>{data.medicine_name}</p>
+        </section>
+      </article>
+    </CenterMain>
+  );
 }
