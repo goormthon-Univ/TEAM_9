0afb5f322ede3875d660f34ce2183575dd394b1b
lybell <futireflugel@gmail.com>
2023-11-16 18:31:36 +0900

    :art: 메인 페이지 가이드 작성
    병영님 여기다가 넣어주시면 됩니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 import DiseasePage from "./pages/DiseasePage";
 import DiseaseDetailPage from "./pages/DiseaseDetailPage";
 import MedicinePage from "./pages/MedicinePage";
 import MedicineDetailPage from "./pages/MedicineDetailPage";
 import HealthGuidePage from "./pages/HealthGuidePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function Rounter() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route exact path="/" element={<MainPage />} />
         <Route path="disease">
           <Route path=":diseaseId" element={<DiseaseDetailPage />} />
           <Route path="" element={<DiseasePage />} />
@@ -20,7 +23,9 @@ function Rounter() {
         <Route path="healthGuide">
           <Route path="food" element={<HealthGuidePage content="food" />} />
           <Route path="living" element={<HealthGuidePage content="living" />} />
+          <Route path="" element={<HealthGuidePage content="food" />} />
         </Route>
+        <Route path={"*"} element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
