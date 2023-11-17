b6b844caa5d159b11d6ba7ab1f3bf19849217cce
lybell <futireflugel@gmail.com>
2023-11-17 21:33:18 +0900

    :hammer: Vite config에 배포 백엔드 api 프록시 추가

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 
 async function enableMocking() {
+  //실서버와 연동시 //return;의 주석 지워서 테스트해주세요
+  //return;
   if (import.meta.env.MODE !== "development") return;
   const { worker } = await import("./mocks/browser");
   return worker.start({ onUnhandledRequest: "bypass" });
