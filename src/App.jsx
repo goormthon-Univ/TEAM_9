b21bdbee8f429a616ffe60b5f7f06bdfca735273
Lee Geun Woo <gamebdy@naver.com>
2023-11-18 03:19:37 +0900

    커뮤니티만 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,58 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavigationBar from "./components/NavigationBar.jsx";
+import Footer from "./components/Footer.jsx";
+import DiseasePage from "./pages/DiseasePage";
+import DiseaseDetailPage from "./pages/DiseaseDetailPage";
+import MedicinePage from "./pages/MedicinePage";
+import MedicineDetailPage from "./pages/MedicineDetailPage";
+import HealthGuidePage from "./pages/HealthGuidePage";
+import CommunityPage from "./pages/CommunityPage";
+import CommunityDetailPage from "./pages/CommunityPage/CommunityDetailPage.jsx";
+import WritePage from "./pages/CommunityPage/WritePage.jsx";
+
+import NotFoundPage from "./pages/NotFoundPage";
+import { Home } from "./pages/Home/Home.jsx";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <NavigationBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="disease">
+          <Route path=":diseaseId" element={<DiseaseDetailPage />} />
+          <Route path="" element={<DiseasePage />} />
+        </Route>
+        <Route path="medicine">
+          <Route path=":medicineId" element={<MedicineDetailPage />} />
+          <Route path="" element={<MedicinePage />} />
+        </Route>
+        <Route path="healthGuide">
+          <Route path="food" element={<HealthGuidePage content="food" />} />
+          <Route path="living" element={<HealthGuidePage content="living" />} />
+          <Route path="" element={<HealthGuidePage content="food" />} />
+        </Route>
+        <Route exact path="community">
+          <Route
+            path="mainboard"
+            element={<CommunityPage content="mainboard" />}
+          />
+          <Route path="consult" element={<CommunityPage content="consult" />} />
+          <Route path="review" element={<CommunityPage content="review" />} />
+          <Route path="write" element={<WritePage content="write" />} />
+          <Route path=":postNumber" element={<CommunityDetailPage />} />
+          {/* <Route path="1" element={<CommunityDetailPage />} /> */}
+          <Route path="" element={<CommunityPage content="mainboard" />} />
+        </Route>
+        {/* <Route path={"*"} element={<NotFoundPage />} /> */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
 function App() {
-  return <div>Hello, world!</div>;
+  return <Router />;
 }
 
 export default App;
---
+++ src/pages/CommunityPage/Community.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import styles from "styled-components";
+import CommunityBoard from "./CommunityBoard.jsx";
+
+const ButtonContainer = styles.div`
+  display: flex;
+  gap: 28px;
+`;
+
+const Button = styles.span(({ $activate = false } = {}) => ({
+  display: "inline-block",
+  height: "60px",
+  padding: "18px 32px",
+  boxSizing: "border-box",
+  border: $activate ? "none" : "2px solid #FF6B00",
+  borderRadius: "32px",
+  backgroundColor: $activate ? "#FF6B00" : "white",
+  color: $activate ? "white" : "black",
+  cursor: "pointer",
+}));
+export default function Community({ content }) {
+  return (
+    <>
+      <ButtonContainer>
+        <Link to="/community/mainboard">
+          <Button className="button" $activate={content === "mainboard"}>
+            전체 게시판
+          </Button>
+        </Link>
+        <Link to="/community/consult">
+          <Button className="button" $activate={content === "consult"}>
+            의료 상담
+          </Button>
+        </Link>
+        <Link to="/community/review">
+          <Button className="button" $activate={content === "review"}>
+            의약품, 영양제 리뷰
+          </Button>
+        </Link>
+      </ButtonContainer>
+      <CommunityBoard tap={content} />
+    </>
+  );
+}
