a3e154f3f178d26a7926767f191ac854de7211e2
Lee Geun Woo <gamebdy@naver.com>
2023-11-18 23:11:41 +0900

    ✨ 커뮤니티 글쓰기/댓글 추가구현

--- src/pages/CommunityPage/WritePage.jsx
+++ src/pages/CommunityPage/WritePage.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
 import CommunityRules from "../../components/CommunityRules";
 import CenterMain from "../../components/CenterMain";
 
@@ -51,9 +53,28 @@ const WriteButtonContainer = styled.div`
   justify-content: flex-end;
 `;
 
-const WritePage = () => {
+const WritePage = (updatePostList) => {
+  const navigate = useNavigate();
+
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    const newPost = {
+      number: 3, // 예시로 새 글의 번호를 지정합니다.
+      title: event.target.elements.title.value,
+      writer: "그누", // 실제로는 로그인 정보 또는 사용자 입력을 받아와야 합니다.
+      views: 0, // 조회수 초기값
+      date: new Date().toISOString().split("T")[0], // 글 작성일
+      contentType: event.target.contentType.value,
+      content: event.target.content.value,
+      comments: [],
+    };
+
+    // 글 목록 갱신 함수 호출
+    updatePostList(newPost);
+
+    // 글 작성 후 mainboard로 이동
+    navigate("/community/mainboard");
   };
 
   return (
@@ -62,13 +83,17 @@ const WritePage = () => {
         <CommunityRules />
         <h3> 게시글 작성</h3>
         <WriteForm onSubmit={handleSubmit}>
-          <Dropdown>
+          <Dropdown name="contentType">
             <option value="medicalConsultation">의료상담</option>
             <option value="medicineNutritionReview">의약품/영양제 리뷰</option>
           </Dropdown>
-          <Input type="text" placeholder="제목을 입력해주세요." />
-          <Input type="text" placeholder="질병명을 입력해주세요." />
-          <TextArea placeholder="내용을 입력해주세요" />
+          <Input type="text" name="title" placeholder="제목을 입력해주세요." />
+          <Input
+            type="text"
+            name="disease"
+            placeholder="질병명을 입력해주세요."
+          />
+          <TextArea name="content" placeholder="내용을 입력해주세요" />
           <WriteButtonContainer>
             <SubmitButton type="submit">글쓰기</SubmitButton>
           </WriteButtonContainer>
