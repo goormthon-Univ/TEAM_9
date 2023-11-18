0e7fe22fbbac9840696fd840af9c6aac5d560ebb
Beongyoung Park <sergiopark10373@likelion.org>
2023-11-18 20:01:31 +0900

    :fire: PersonalRecommand
    url : /medicine/recommand

--- src/pages/RecommandPage/PersonalRecommand.jsx
+++ src/pages/RecommandPage/PersonalRecommand.jsx
@@ -1,44 +1,87 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const CardWrapper = styled.div`
+  border: 2px solid #ccc;
+  padding: 10px;
+  text-align: center;
+  cursor: pointer;
+
+  ${(props) =>
+    props.selected &&
+    `
+    border-color: blue; /* Change to your desired outline color */
+  `}
+`;
 
 const PersonalRecommand = () => {
-  const [selectedDivs, setSelectedDivs] = useState([]);
+  const [selectedCards, setSelectedCards] = useState([]);
 
-  const handleDivClick = (divId) => {
-    if (selectedDivs.includes(divId)) {
-      setSelectedDivs(selectedDivs.filter((id) => id !== divId));
-    } else if (selectedDivs.length < 4) {
-      setSelectedDivs([...selectedDivs, divId]);
-    }
+  const handleCardSelect = (cardId) => {
+    setSelectedCards((prevSelected) => {
+      if (prevSelected.includes(cardId)) {
+        // If card is already selected, remove it
+        return prevSelected.filter((id) => id !== cardId);
+      } else {
+        // If less than 4 cards are selected, add the new selection
+        if (prevSelected.length < 4) {
+          return [...prevSelected, cardId];
+        }
+        // If 4 cards are already selected, return the current selection
+        return prevSelected;
+      }
+    });
+  };
+
+  const handleSubmit = () => {
+    // Handle the submission of selected cards
+    console.log("Selected Cards:", selectedCards);
+    // Add your logic to send the selected cards to the server or perform other actions
   };
 
   return (
     <div>
-      <div
-        onClick={() => handleDivClick(1)}
-        className={selectedDivs.includes(1) ? "selected" : ""}
-      >
-        Div 1
-      </div>
-      <div
-        onClick={() => handleDivClick(2)}
-        className={selectedDivs.includes(2) ? "selected" : ""}
-      >
-        Div 2
-      </div>
-      <div
-        onClick={() => handleDivClick(3)}
-        className={selectedDivs.includes(3) ? "selected" : ""}
-      >
-        Div 3
-      </div>
-      <div
-        onClick={() => handleDivClick(4)}
-        className={selectedDivs.includes(4) ? "selected" : ""}
-      >
-        Div 4
-      </div>
+      <GridWrapper>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((id) => (
+          <Card
+            key={id}
+            id={id}
+            selected={selectedCards.includes(id)}
+            onSelect={handleCardSelect}
+          />
+        ))}
+      </GridWrapper>
+      <SubmitButton onClick={handleSubmit}>Submit</SubmitButton>
     </div>
   );
 };
+
+const GridWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+`;
+
+const Card = ({ id, selected, onSelect }) => {
+  const handleClick = () => {
+    onSelect(id);
+  };
+
+  return (
+    <CardWrapper selected={selected} onClick={handleClick}>
+      Card {id}
+    </CardWrapper>
+  );
+};
+
+const SubmitButton = styled.button`
+  margin-top: 10px;
+  padding: 10px;
+  background-color: #4caf50;
+  color: white;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+`;
 
 export default PersonalRecommand;
